8d1dfdb7e4490769d7fe733259d755d09dfcbd6c
idan <idanbe7@gmail.com>
2022-11-26 19:37:43 +0200

    updating the validator (fixing small bugs)

--- validator.py
+++ validator.py
@@ -19,24 +19,51 @@ class Validator:
         :return: True if the exercise is valid, False otherwise
         """
         index = 1
-        while self.exercise[index] != "\0":
+        while self.exercise[index + 1] != "\0":
+            print(self.exercise[index], index)
             before = self.exercise[index - 1]
             after = self.exercise[index + 1]
-            # check if the char is a valid char
-            if self.exercise[index] not in self.valid_chars:
+            # check if the char is a valid char or an operand
+            char = self.exercise[index]
+            if (char not in self.valid_chars) and (not self.is_operand(char)):
                 return False
             # check if the char is a dot and the char before and after the dot is an operand
-            if self.exercise[index] == ".":
-                return self.is_operand(before) and self.is_operand(after)
+            if char == ".":
+                if not self.is_operand(before) and self.is_operand(after):
+                    return False
             # calls the validate_same_before_after function if the char is in the same_before_after list
-            if self.exercise[index] in self.same_before_after:
-                self.validate_same_before_after(before, after)
+            if char in self.same_before_after:
+                if not self.validate_same_before_after(before, after):
+                    return False
             # calls the validate_different_before_after function if the char is in the different_before_after list
-            if self.exercise[index] in self.different_before_after:
-                self.validate_different_before_after(index, before, after)
+            if char in self.different_before_after:
+                if not self.validate_different_before_after(index, before, after):
+                    return False
             # calls the validate_brackets function if the char is a bracket
-            if self.exercise[index] == "(" or self.exercise[index] == ")":
-                self.validate_brackets(index, before, after)
+            if char == "(" or char == ")":
+                if not self.validate_brackets(index, before, after):
+                    return False
+            # calls the validate_operand function if the char is an operand
+            if self.is_operand(char):
+                if not self.validate_operand(before, after):
+                    return False
+            index += 1
+        return True
+
+    def validate_operand(self, before: str, after: str) -> bool:
+        """
+        validate the exercise if the char is an operand
+        :param before: the char before the index
+        :param after: the char after the index
+        :return: True if the exercise is valid, False otherwise
+        """
+        # check if the char after the index is an opening bracket or a tilda
+        if after == "(" or after == "~":
+            return False
+        # check if the char before the index is a closing bracket or a factorial
+        if before == ")" or before == "!":
+            return False
+        return True
 
     def validate_different_before_after(self, index: int, before: str, after: str) -> bool:
         """
@@ -55,8 +82,6 @@ class Validator:
         # if the char is a tilda
         if self.exercise[index] == "~":
             return self.validate_tilda(before, after)
-        # otherwise, the exercise is not valid
-        return False
 
     def validate_tilda(self, before: str, after: str) -> bool:
         """
@@ -66,11 +91,10 @@ class Validator:
         :return: True if the exercise is valid, False otherwise
         """
         # check if the char before the index is an operator except factorial
-        if self.is_operand(before) and before != "!" and before != "~":
-            return True
-        # check if the char after the index is an operand or a bracket or a minus
-        if self.is_operand(after) or after == "(" or after == "-":
-            return True
+        if self.is_operand(before) or before != "!" or before != "~":
+            # check if the char after the index is an operand or a bracket or a minus
+            if self.is_operand(after) or after == "(" or after == "-":
+                return True
         return False
 
     def validate_factorial(self, before: str, after: str) -> bool:
@@ -82,10 +106,9 @@ class Validator:
         """
         # check if the char before the index is an operand or a bracket or a factorial
         if self.is_operand(before) or before == ")" or before == "!":
-            return True
-        # check if the char after the index is an operator
-        if self.is_operator(after):
-            return True
+            # check if the char after the index is an operator
+            if self.is_operator(after):
+                return True
         return False
 
     def validate_minus(self, before: str, after: str) -> bool:
@@ -97,10 +120,9 @@ class Validator:
         """
         # check if the char before the index is an operator or a bracket or a factorial or a minus
         if self.is_operand(before) or before == ")" or before == "!" or before == "-":
-            return True
-        # check if the char after the index is an operand or a bracket or a minus or a tilda
-        if self.is_operand(after) or after == "(" or after == "-" or after == "~":
-            return True
+            # check if the char after the index is an operand or a bracket or a minus or a tilda
+            if self.is_operand(after) or after == "(" or after == "-" or after == "~":
+                return True
         return False
 
     def validate_brackets(self, index: int, before: str, after: str) -> bool:
@@ -117,7 +139,6 @@ class Validator:
         # if the char is a closing bracket
         if self.exercise[index] == ")":
             return self.validate_closing_bracket(before, after)
-        return False
 
     def validate_closing_bracket(self, before: str, after: str) -> bool:
         """
@@ -127,11 +148,12 @@ class Validator:
         :return: True if the exercise is valid, False otherwise
         """
         # check if the char before the index is an operand or a closing bracket or a factorial
-        if self.is_operand(before) or before == ")" or before == "!":
-            return True
-        # check if the char after the index is in the valid chars list
-        if after in self.valid_chars:
-            return True
+        # and not an opening bracket
+        if (self.is_operand(before) or before == ")" or before == "!") and before != "(":
+            # check if the char after the index is in the valid chars list or not an opening bracket
+            # and not an opening bracket
+            if (after in self.valid_chars) and after != "(":
+                return True
         return False
 
     def validate_opening_bracket(self, before: str, after: str) -> bool:
@@ -142,11 +164,11 @@ class Validator:
         :return: True if the exercise is valid, False otherwise
         """
         # check if the char before the index is an operator or an opening bracket and not a factorial
-        if (self.is_operator(before) and before != "!") or before == "(":
-            return True
-        # check if the char after the index is an operand or an opening bracket or a tilda
-        if self.is_operand(after) or after == "(" or after == "~":
-            return True
+        # and not a closing bracket
+        if (self.is_operator(before) or before != "!" or before == "(") and before != ")":
+            # check if the char after the index is an operand or an opening bracket or a tilda and not a closing bracket
+            if (self.is_operand(after) or after == "(" or after == "~") and after != ")":
+                return True
         return False
 
     def validate_same_before_after(self, before: str, after: str) -> bool:
@@ -157,16 +179,11 @@ class Validator:
         :param after: the char after the index
         :return: True if the exercise is valid, False otherwise
         """
-        # check if the char before and after the index is an operand
-        if self.is_operand(before) or self.is_operand(after):
-            return True
-        # check if the char before and after the index is a bracket
-        if before == ")" and after == "(":
-            return True
-        # check if the char before and after the index is a factorial
-        if before == "!":
-            return True
-        # otherwise, the exercise is not valid
+        # check if the char before the index is an operand or a closing bracket or a factorial
+        if before == ")" or before == "!" or self.is_operand(before):
+            # check if the char after the index is an operand or an opening bracket
+            if after == "(" or self.is_operand(after):
+                return True
         return False
 
     def is_operator(self, char: str) -> bool:
@@ -198,6 +215,7 @@ class Validator:
 # average: after: operand, (, -, ~ before: operand, ), factorial
 # tilda: after: operand, (, - before: operator (except factorial and tilda)
 # factorial: after: operator before: operand, factorial, )
-# opening bracket: after: operand, (, ~ before: operator (except factorial), (
-# closing bracket: after: every valid char before: ), operand, factorial
+# opening bracket: after: operand, (, ~ before: operator (except factorial), (, can't have closing bracket in both sides
+# closing bracket: after: every valid char before: ), operand, factorial, can't have opening bracket in both sides
 # dot: after: operand before: operand
+# operand: after: can't be ( and ~ before: can't be ) and !
