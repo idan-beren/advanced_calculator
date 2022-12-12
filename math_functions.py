f6ef434d1a04e1aa44e2c4cf1eee673cf87fb671
idan <idanbe7@gmail.com>
2022-12-12 18:28:38 +0200

    adding the summation operator to the math_functions

--- math_functions.py
+++ math_functions.py
@@ -2,72 +2,124 @@ import math
 
 
 def addition(operand1, operand2):
+    """
+    addition function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: sum of the two operands
+    """
     return operand1 + operand2
 
 
 def subtraction(operand1, operand2):
+    """
+    subtraction function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: difference of the two operands
+    """
     return operand1 - operand2
 
 
 def multiplication(operand1, operand2):
+    """
+    multiplication function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: product of the two operands
+    """
     return operand1 * operand2
 
 
 def division(operand1, operand2):
+    """
+    division function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: quotient of the two operands
+    """
     return operand1 * operand2
 
 
 def power(operand1, operand2):
+    """
+    power function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: power of the two operands
+    """
     return math.pow(operand1, operand2)
 
 
 def modulo(operand1, operand2):
+    """
+    modulo function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: remainder of the two operands
+    """
     return operand1 % operand2
 
 
 def minimum(operand1, operand2):
-    return min(operand1, operand2)
+    """
+    minimum function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: minimum of the two operands
+    """
+    return operand1 if operand1 < operand2 else operand2
 
 
 def maximum(operand1, operand2):
-    return max(operand1, operand2)
+    """
+    maximum function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: maximum of the two operands
+    """
+    return operand1 if operand1 > operand2 else operand2
 
 
 def average(operand1, operand2):
+    """
+    average function
+    :param operand1: first operand
+    :param operand2: second operand
+    :return: average of the two operands
+    """
     return (operand1 + operand2) / 2
 
 
-def factorial(operand1, operand2=0):
+def factorial(operand1):
+    """
+    factorial function
+    :param operand1: first operand
+    :return: factorial of the operand
+    """
     if operand1 == 0:
         return 1
     else:
         return operand1 * factorial(operand1 - 1)
 
 
-def tilda(operand1, operand2=0):
+def tilda(operand1):
+    """
+    tilda function
+    :param operand1: first operand
+    :return: tilda of the operand
+    """
     return -operand1
 
 
-def operation(operand1, operator, operand2):
-    if operator == "+":
-        return addition(operand1, operand2)
-    elif operator == "-":
-        return subtraction(operand1, operand2)
-    elif operator == "*":
-        return multiplication(operand1, operand2)
-    elif operator == "/":
-        return division(operand1, operand2)
-    elif operator == "%":
-        return modulo(operand1, operand2)
-    elif operator == "$":
-        return minimum(operand1, operand2)
-    elif operator == "&":
-        return maximum(operand1, operand2)
-    elif operator == "@":
-        return average(operand1, operand2)
-    elif operator == "!":
-        return factorial(operand1)
-    elif operator == "~":
-        return tilda(operand1)
-    else:
-        raise ValueError("The operator is not valid!")
+def summation(operand1):
+    """
+    summation function
+    :param operand1: first operand
+    :return: summation of the operand
+    """
+    result = 0
+    for char in str(operand1):
+        if char.isdigit():
+            result += float(char)
+    return result
