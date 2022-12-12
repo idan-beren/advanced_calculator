3e39f4afb42e926667e23f82f60fb99c2c5cf57f
idan <idanbe7@gmail.com>
2022-12-12 18:25:23 +0200

    updating handler and validator classes to be generic.

--- handler.py
+++ handler.py
@@ -1,169 +1,49 @@
+from validator import *
+from configuration import *
+
+
 class Handler(object):
-    """class to check if the exercise is valid and to handle the exercise"""
-    def __init__(self, exercise: str):
-        # list of the operators
-        self.operators = ['+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!']
-        # the exercise to handle plus the end of string character
-        self.exercise = exercise + "\0"
-        # check if the exercise is valid
-        self.is_valid()
-        # remove spaces from the exercise
+    """class to check if the expression is valid and to handle the expression"""
+    def __init__(self, expression: str):
+        self.expression = list(expression)
+        self.validate = Validator(self.expression)
+        self.validate.validate()
         self.remove_spaces()
-        # print the exercise
-        print(self.exercise[:-1])
+        self.empty_expression()
+        self.validate.expression = self.expression
+        self.validate.validate_expression()
+        self.merge_operands()
+        print(self.expression)
 
-    def is_operator(self, char: str) -> bool:
-        """
-        check if the char is an operator
-        :param char: the char to check
-        :return: True if the char is an operator, False otherwise
-        """
-        return char in self.operators
+    def before_after(self, index: int):
+        """return the char before and after the given index"""
+        return self.expression[index - 1], self.expression[index + 1]
 
-    def is_operand(self, string: str) -> bool:
-        """
-        check if the string is an operand
-        :param string: the string to check
-        :return: True if the string is an operand, False otherwise
-        """
-        return string.isnumeric()
+    def empty_expression(self):
+        """check if the expression is empty"""
+        if len(self.expression) == 0:
+            raise ValueError("Empty expression")
 
     def remove_spaces(self):
-        """remove spaces from the exercise if in one side is an operator and in the other side is an operand"""
+        """remove spaces from the expression"""
         index = 1
-        while self.exercise[index] != "\0":
-            # if the char is a space
-            if self.exercise[index] == " ":
-                # if the char before the space is an operator and the char after the space is an operand
-                if self.is_operator(self.exercise[index - 1]) and self.is_operand(self.exercise[index + 1]):
-                    self.erase_spaces(index)
-                # if the char before the space is an operand and the char after the space is an operator
-                elif self.is_operand(self.exercise[index - 1]) and self.is_operator(self.exercise[index + 1]):
-                    self.erase_spaces(index)
-                # if the char before the space is a space and the char after the space is a space
-                elif self.exercise[index - 1] == " " or self.exercise[index + 1] == " ":
-                    self.erase_spaces(index)
-                # if the char before the space is an opening bracket and the char after the space is a closing bracket
-                elif self.exercise[index - 1] == "(" or self.exercise[index + 1] == ")":
-                    self.erase_spaces(index)
+        while index < len(self.expression) - 1:
+            before, after = self.before_after(index)
+            if self.expression[index] == SPACE:
+                if not (before.isdigit() and after.isdigit()):
+                    del self.expression[index]
                 else:
-                    index += 1
+                    raise ValueError("Invalid expression")
             else:
                 index += 1
+        self.expression = [index for index in self.expression if index != SPACE]
 
-    def erase_spaces(self, index: int):
-        """erase the spaces from the exercise"""
-        self.exercise = self.exercise[:index] + self.exercise[index + 1:]
-
-    def is_valid(self):
-        """
-        check if the exercise is valid
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the exercise is empty
-        if self.exercise == '\0':
-            raise ValueError("The exercise is empty!")
-        # check if the exercise contains incorrect characters
-        if self.incorrect_char() is False:
-            raise ValueError("The exercise contains incorrect characters!")
-        # check if the exercise contains only operators
-        if self.only(self.is_operator) is True:
-            raise ValueError("The exercise contains only operators!")
-        # check if the exercise contains only operands
-        if self.only(self.is_operand) is True:
-            raise ValueError("The exercise contains only operands!")
-        # check if the exercise contains an operator
-        if self.contains(self.is_operator) is False:
-            raise ValueError("The exercise contains no operator!")
-        # check if the exercise contains an operand
-        if self.contains(self.is_operand) is False:
-            raise ValueError("The exercise contains no operand!")
-        # check if the brackets are correct
-        if self.correct_brackets() is False:
-            raise ValueError("The brackets are incorrect!")
-        # check if the exercise ends with an operator except the factorial operator
-        if self.is_operator(self.exercise[-2]) and self.exercise[-2] != '!':
-            raise ValueError("The exercise ends with an operator!")
-        # check if the exercise starts with an operator except the minus and tilda operators
-        if self.is_operator(self.exercise[0]) and self.exercise[0] != '-' and self.exercise[0] != '~':
-            raise ValueError("The exercise starts with an operator!")
-        # check if the exercise contains operators in a row except the minus operator
-        if self.operators_in_a_row() is True:
-            raise ValueError("The exercise contains operators in a row!")
-        # check if the exercise contains an empty brackets
-        if self.empty_brackets() is True:
-            raise ValueError("The exercise contains an empty brackets!")
-
-    def incorrect_char(self) -> bool:
-        """
-        check if the exercise contains incorrect characters
-        :return: True if the exercise contains incorrect characters, False otherwise
-        """
-        for char in self.exercise[:-1]:
-            # if the char is not a digit, a space, an operator or a bracket
-            if (not self.is_operator(char)) and (not self.is_operand(char)) \
-                    and char != '(' and char != ')' and char != ' ':
-                return False
-        return True
-
-    def contains(self, function: callable) -> bool:
-        """
-        check if the exercise contains something
-        :param function: the function to check
-        :return: True if the exercise contains something, False otherwise
-        """
-        for char in self.exercise[:-1]:
-            if function(char):
-                return True
-        return False
-
-    def only(self, function: callable) -> bool:
-        """
-        check if the exercise contains only something
-        :param function: the function to check
-        :return: True if the exercise contains only something, False otherwise
-        """
-        for char in self.exercise[:-1]:
-            if not function(char):
-                return False
-        return True
-
-    def correct_brackets(self) -> bool:
-        """
-        check if the brackets are correct
-        :return: True if the brackets are correct, False otherwise
-        """
-        brackets = []
-        for char in self.exercise[:-1]:
-            if char == '(':
-                brackets.append(char)
-            elif char == ')':
-                if len(brackets) == 0:
-                    return False
-                brackets.pop()
-        return len(brackets) == 0
-
-    def operators_in_a_row(self) -> bool:
-        """
-        check if the exercise contains operators in a row except the minus operator
-        :return: True if the exercise contains operators in a row except the minus operator, False otherwise
-        """
+    def merge_operands(self):
+        """merge the operands"""
         index = 0
-        while self.exercise[index] != "\0":
-            if self.is_operator(self.exercise[index]):
-                if self.is_operator(self.exercise[index + 1]) and self.exercise[index + 1] != '-':
-                    return True
-            index += 1
-        return False
-
-    def empty_brackets(self) -> bool:
-        """
-        check if the exercise contains empty brackets
-        :return: True if the exercise contains empty brackets, False otherwise
-        """
-        index = 0
-        while self.exercise[index] != "\0":
-            if self.exercise[index] == '(' and self.exercise[index + 1] == ')':
-                return True
-            index += 1
-        return False
+        while index < len(self.expression) - 1:
+            if self.expression[index].isdigit() and self.expression[index + 1].isdigit():
+                self.expression[index] += self.expression[index + 1]
+                del self.expression[index + 1]
+            else:
+                index += 1
--- validator.py
+++ validator.py
@@ -1,223 +1,107 @@
-class Validator:
-    def __init__(self, exercise: str):
-        # the exercise to validate
-        self.exercise = exercise
-        # the valid chars
-        self.valid_chars = ['+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!', '(', ')', '.']
-        # the operators with the same char before and after
-        self.same_before_after = ['+', '*', '/', '^', '%', '$', '&', '@']
-        # the operators with the different char before and after
-        self.different_before_after = ['-', '~', '!']
-        # brackets
-        self.brackets = ['(', ')']
-        # the operators
-        self.operators = ['+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!']
+from configuration import *
 
-    def validate(self) -> bool:
-        """
-        validate the exercise according to the rules at the bottom of the file
-        :return: True if the exercise is valid, False otherwise
-        """
+
+class Validator(object):
+    def __init__(self, expression: list):
+        self.expression = expression
+
+    def validate(self):
+        """check if the expression is valid"""
+        self.vlidate_chars()
+
+    def vlidate_chars(self):
+        """check if the chars are valid"""
+        for char in self.expression:
+            self.is_valid_char(char)
+
+    def is_valid_char(self, char: str):
+        """check if the char is valid"""
+        if not (char in VALID_CHARS or char.isdigit()):
+            raise ValueError("Invalid char: " + char)
+
+    def before_after(self, index: int):
+        """return the char before and after the given index"""
+        return self.expression[index - 1], self.expression[index + 1]
+
+    def validate_expression(self):
         index = 1
-        while self.exercise[index + 1] != "\0":
-            before = self.exercise[index - 1]
-            after = self.exercise[index + 1]
-            # check if the char is a valid char or an operand
-            char = self.exercise[index]
-            if (char not in self.valid_chars) and (not self.is_operand(char)):
-                return False
-            # check if the char is a dot and the char before and after the dot is an operand
-            if char == ".":
-                if not (self.is_operand(before) and self.is_operand(after)):
-                    return False
-            # calls the validate_same_before_after function if the char is in the same_before_after list
-            if char in self.same_before_after:
-                if not self.validate_same_before_after(before, after):
-                    return False
-            # calls the validate_different_before_after function if the char is in the different_before_after list
-            if char in self.different_before_after:
-                if not self.validate_different_before_after(index, before, after):
-                    return False
-            # calls the validate_brackets function if the char is a bracket
-            if char in self.brackets:
-                if not self.validate_brackets(index, before, after):
-                    return False
-            # calls the validate_operand function if the char is an operand
-            if self.is_operand(char):
-                if not self.validate_operand(before, after):
-                    return False
+        while index < len(self.expression) - 1:
+            before, after = self.before_after(index)
+            item = self.expression[index]
+            if item in OPENING_BRACKETS + CLOSING_BRACKETS:
+                self.validate_brackets(index, before, after)
+            elif item == MINUS:
+                self.validate_minus(before, after)
+            elif item in OPERATORS:
+                self.validate_operators(index, before, after)
+            elif item.isdigit():
+                self.validate_operands(before, after)
+            elif item == DOT:
+                self.validate_dots(before, after)
             index += 1
-        return True
 
-    def validate_operand(self, before: str, after: str) -> bool:
-        """
-        validate the exercise if the char is an operand
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char after the index is an opening bracket or a tilda
-        if after == "(" or after == "~":
-            return False
-        # check if the char before the index is a closing bracket or a factorial
-        if before == ")" or before == "!":
-            return False
-        return True
+    def validate_brackets(self, index: int, before: str, after: str):
+        if self.expression[index] in OPENING_BRACKETS:
+            self.validate_opening_bracket(before, after)
+        elif self.expression[index] in CLOSING_BRACKETS:
+            self.validate_closing_bracket(before, after)
 
-    def validate_different_before_after(self, index: int, before: str, after: str) -> bool:
-        """
-        validate the exercise if the char is in the different_before_after list
-        :param index: the index of the char
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # if the char is a factorial
-        if self.exercise[index] == "!":
-            return self.validate_factorial(before, after)
-        # if the char is a minus
-        if self.exercise[index] == "-":
-            return self.validate_minus(before, after)
-        # if the char is a tilda
-        if self.exercise[index] == "~":
-            return self.validate_tilda(before, after)
+    def validate_opening_bracket(self, before: str, after: str):
+        if (before in OPERATORS and self.check_operand_side(before) != LEFT) or before in OPENING_BRACKETS:
+            if after.isdigit() or after in OPENING_BRACKETS or self.check_operand_side(after) == RIGHT or after == MINUS:
+                return
+        raise ValueError("Invalid expression 1")
 
-    def validate_tilda(self, before: str, after: str) -> bool:
-        # tilda: after: operand, (, - before: operator (except factorial and tilda)
-        """
-        validate the exercise if the char is a tilda
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operator except factorial and tilda
-        if self.is_operator(before) and before != "!" and before != "~":
-            # check if the char after the index is an operand or a bracket or a minus
-            if self.is_operand(after) or after == "(" or after == "-":
-                return True
-        return False
+    def validate_closing_bracket(self, before: str, after: str):
+        if before.isdigit() or before in CLOSING_BRACKETS or self.check_operand_side(before) == LEFT:
+            if (after in OPERATORS and self.check_operand_side(after) != RIGHT) or after in CLOSING_BRACKETS:
+                return
+        raise ValueError("Invalid expression 2")
 
-    def validate_factorial(self, before: str, after: str) -> bool:
-        # factorial: after: operator (except tilda) before: operand, factorial, )
-        """
-        validate the exercise if the char is a factorial
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operand or a closing bracket or a factorial
-        if self.is_operand(before) or before == ")" or before == "!":
-            # check if the char after the index is an operator except tilda
-            if self.is_operator(after) and after != '~':
-                return True
-        return False
+    def validate_minus(self, before: str, after: str):
+        if before.isdigit() or before in CLOSING_BRACKETS + OPENING_BRACKETS or before in OPERATORS:
+            if after.isdigit() or after in OPENING_BRACKETS or after == MINUS or self.check_operand_side(after) == RIGHT:
+                return
+        raise ValueError("Invalid expression 9")
 
-    def validate_minus(self, before: str, after: str) -> bool:
-        # subtraction: after: operand, (, -, ~ before: operand, operator, (, )
-        """
-        validate the exercise if the char is a minus
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operator or a bracket or an operand
-        if self.is_operand(before) or before in self.brackets or self.is_operator(before):
-            # check if the char after the index is an operand or a bracket or a minus or a tilda
-            if self.is_operand(after) or after == "(" or after == "-" or after == "~":
-                return True
-        return False
+    def validate_operators(self, index: int, before: str, after: str):
+        item = self.expression[index]
+        if OPERAND_SIDE[item] == LEFT:
+            self.validate_left_operator(before, after)
+        elif OPERAND_SIDE[item] == RIGHT:
+            self.validate_right_operator(before, after)
+        elif OPERAND_SIDE[item] == BOTH:
+            self.validate_both_operator(before, after)
 
-    def validate_brackets(self, index: int, before: str, after: str) -> bool:
-        """
-        validate the exercise if the char is a bracket
-        :param index: the index of the char
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # if the char is an opening bracket
-        if self.exercise[index] == "(":
-            return self.validate_opening_bracket(before, after)
-        # if the char is a closing bracket
-        if self.exercise[index] == ")":
-            return self.validate_closing_bracket(before, after)
+    def validate_left_operator(self, before: str, after: str):
+        if before.isdigit() or before in CLOSING_BRACKETS or self.check_operand_side(before) == LEFT:
+            if (after in OPERATORS and self.check_operand_side(after) != RIGHT) or after in CLOSING_BRACKETS:
+                return
+        raise ValueError("Invalid expression 3")
 
-    def validate_closing_bracket(self, before: str, after: str) -> bool:
-        # closing bracket: after: operator (except tilda), ) before: ), operand, factorial
-        """
-        validate the exercise if the char is a closing bracket
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operand or a closing bracket or a factorial
-        if self.is_operand(before) or before == ")" or before == "!":
-            # check if the char after the index is an operator (except tilda) or a closing bracket
-            if (self.is_operator(after) and after != "~") or after == ")":
-                return True
-        return False
+    def validate_right_operator(self, before: str, after: str):
+        if (before in OPERATORS and self.check_operand_side(before) != RIGHT and self.check_operand_side(before) != LEFT) or before in OPENING_BRACKETS:
+            if after.isdigit() or after in OPENING_BRACKETS or after == MINUS:
+                return
+        raise ValueError("Invalid expression 4")
 
-    def validate_opening_bracket(self, before: str, after: str) -> bool:
-        # opening bracket: after: operand, (, ~, - before: operator (except factorial), (
-        """
-        validate the exercise if the char is an opening bracket
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operator (except factorial) or an opening bracket
-        if (self.is_operator(before) and before != "!") or before == "(":
-            # check if the char after the index is an operand or an opening bracket or a tilda or a minus
-            if self.is_operand(after) or after == "(" or after == "~" or after == "-":
-                return True
-        return False
+    def validate_both_operator(self, before: str, after: str):
+        if before in CLOSING_BRACKETS or self.check_operand_side(before) == LEFT or before.isdigit():
+            if after in OPENING_BRACKETS or after.isdigit() or after == MINUS or self.check_operand_side(after) == RIGHT:
+                return
+        raise ValueError("Invalid expression 5")
 
-    def validate_same_before_after(self, before: str, after: str) -> bool:
-        # same_before_after: after: operand, (, -, ~ before: operand,), factorial
-        """
-        validate the exercise if the char is in the same_before_after list
-        and the char before and after the index is an operand or a bracket or a factorial
-        :param before: the char before the index
-        :param after: the char after the index
-        :return: True if the exercise is valid, False otherwise
-        """
-        # check if the char before the index is an operand or a closing bracket or a factorial
-        if before == ")" or before == "!" or self.is_operand(before):
-            # check if the char after the index is an operand or an opening bracket or minus or tilda
-            if after == "(" or self.is_operand(after) or after == '-' or after == '~':
-                return True
-        return False
+    def validate_operands(self, before: str, after: str):
+        if before in CLOSING_BRACKETS or after in OPENING_BRACKETS:
+            raise ValueError("Invalid expression 6")
+        if self.check_operand_side(before) == LEFT or self.check_operand_side(after) == RIGHT:
+            raise ValueError("Invalid expression 7")
 
-    def is_operator(self, char: str) -> bool:
-        """
-        check if the char is an operator
-        :param char: the char to check
-        :return: True if the char is an operator, False otherwise
-        """
-        return char in self.operators
+    def validate_dots(self, before: str, after: str):
+        if not (before.isdigit() and after.isdigit()):
+            raise ValueError("Invalid expression 8")
 
-    def is_operand(self, string: str) -> bool:
-        """
-        check if the string is an operand
-        :param string: the string to check
-        :return: True if the string is an operand, False otherwise
-        """
-        return string.isnumeric()
-
-# rules of validation: (V) - means valid
-# -----------------------
-# (V) addition: after: operand, (, -, ~ before: operand, ), factorial
-# (V) subtraction: after: operand, (, -, ~ before: operand, operator, (, )
-# (V) multiplication: after: operand, (, -, ~ before: operand, ), factorial
-# (V) division: after: operand, (, -, ~ before: operand, ), factorial
-# (V) power: after: operand, (, -, ~ before: operand, ), factorial
-# (V) modulo: after: operand, (, -, ~ before: operand, ), factorial
-# (V) min: after: operand, (, -, ~ before: operand, ), factorial
-# (V) max: after: operand, (, -, ~ before: operand, ), factorial
-# (V) average: after: operand, (, -, ~ before: operand, ), factorial
-# (V) tilda: after: operand, (, - before: operator (except factorial and tilda)
-# (V) factorial: after: operator (except tilda) before: operand, factorial, )
-# (V) opening bracket: after: operand, (, ~, - before: operator (except factorial), (
-# (V) closing bracket: after: operator (except tilda), ) before: ), operand, factorial
-# (V) dot: after: operand before: operand
-# (V) operand: after: operand, ), operator (except tilda), dot before: operand, (, operator (except factorial), dot
+    def check_operand_side(self, operator: str) -> str:
+        if operator in OPERATORS:
+            return OPERAND_SIDE[operator]
+        return NONE
